7cb688e3654afa4a001e68f96c9e54f70cfabc95
Nditah <nditah@gmail.com>
2019-05-06 18:08:22 +0100

    Update features

--- src/app/services/api-routes.service.ts
+++ src/app/services/api-routes.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 
 export enum GetRoutes {
-  Home          = 'http://localhost:5000/api',
-  Exercises     = 'http://localhost:5000/api/exercises',
-  Foods         = 'http://localhost:5000/api/foods',
-  Images        = 'http://localhost:5000/api/images',
-  Logs          = 'http://localhost:5000/api/logs',
-  Feedbacks     = 'http://localhost:5000/api/feedbacks',
-  Nutrients     = 'http://localhost:5000/api/nutrients',
-  Settings      = 'http://localhost:5000/api/settings',
-  Setup         = 'http://localhost:5000/api/setups',
-  Users         = 'http://localhost:5000/api/users',
+  Home          = 'https://calorie-api.herokuapp.com/api',
+  Exercises     = 'https://calorie-api.herokuapp.com/api/exercises',
+  Foods         = 'https://calorie-api.herokuapp.com/api/foods',
+  Images        = 'https://calorie-api.herokuapp.com/api/images',
+  Logs          = 'https://calorie-api.herokuapp.com/api/logs',
+  Feedbacks     = 'https://calorie-api.herokuapp.com/api/feedbacks',
+  Nutrients     = 'https://calorie-api.herokuapp.com/api/nutrients',
+  Settings      = 'https://calorie-api.herokuapp.com/api/settings',
+  Setup         = 'https://calorie-api.herokuapp.com/api/setups',
+  Users         = 'https://calorie-api.herokuapp.com/api/users',
 }
 
 @Injectable({
